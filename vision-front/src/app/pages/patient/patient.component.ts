6818bbe8e1c395347118c7feeef8266675f3ac1c
Rahul Govindkumar <rahulgovindkumar@gmail.com>
2021-11-27 18:19:50 -0500

    Update As of Now

--- vision-front/src/app/pages/patient/patient.component.ts
+++ vision-front/src/app/pages/patient/patient.component.ts
@@ -14,51 +14,17 @@ export class PatientComponent implements OnInit {
 
   //patientsData: Patient[] = res;
 
-<<<<<<< Updated upstream
-  constructor(private visionService: VisionService) { }
-=======
-  patientsData: Patient[] = [{
-    name: "Jack1",
-    age: 45,
-    gender: "Male",
-    cancerStage: 2,
-    prescribedDrugs: "Drug1, Drug2"
-  },
-  {
-    name: "Jack2",
-    age: 45,
-    gender: "Male",
-    cancerStage: 2,
-    prescribedDrugs: "Drug1, Drug2"
-  },
-  {
-    name: "Jack3",
-    age: 45,
-    gender: "Male",
-    cancerStage: 2,
-    prescribedDrugs: "Drug1, Drug2"
-  },
-  {
-    name: "Jack4",
-    age: 45,
-    gender: "Male",
-    cancerStage: 2,
-    prescribedDrugs: "Drug1, Drug2"
-  },
-  {
-    name: "Jack5",
-    age: 45,
-    gender: "Male",
-    cancerStage: 2,
-    prescribedDrugs: "Drug1, Drug2"
-  }];
+
+  constructor(private visionService: VisionService,private modalService: NgbModal) { }
+
+
 
   selectedRow: Patient = new Patient();
   predictionData: any = { prediction: 4, probability: "67%" };
 
-  constructor(private modalService: NgbModal) { }
->>>>>>> Stashed changes
 
+
+  
   ngOnInit(): void {
 
     this.getPatientData();
